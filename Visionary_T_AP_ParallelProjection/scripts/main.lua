26031c630177adf448dbccb7bcc2b7de08449901
GitLab Runner <gitlab-runner@ip-172-31-13-207.eu-central-1.compute.internal>
2023-12-19 12:08:37 +0000

    Update

--- Visionary_T_AP_ParallelProjection/scripts/main.lua
+++ Visionary_T_AP_ParallelProjection/scripts/main.lua
@@ -1,25 +1,25 @@
 --[[----------------------------------------------------------------------------
 
   Application Name: Visionary_T_AP_ParallelProjection
-  
+
   Summary:
   Sample to show how to get a parallel projection of the scene out of a Depth map image.
-  
+
   Description:
   Set up the camera to take live images continuously and automatically calculate
   pointclouds out of it. First the full depthmap image is shown on the left Viewer. In
   the second viewer to the right, the depthmap image is converted to a pointcloud and shown.
   In the bottom viewer, height map image of the point cloud rotated by a specified angle
   along the Y and X axes is shown.
-  
+
   How to run:
   Start by running the app (F5) or debugging (F7+F10).
   Set a breakpoint on the first row inside the main function to debug step-by-step.
   See the results in the different 3D and 2D viewers on the DevicePage.
-  
+
   More Information:
   If you want to run this app on an emulator some changes are needed to get images.
-    
+
 ------------------------------------------------------------------------------]]
 --Start of Global Scope---------------------------------------------------------
 --Log.setLevel("INFO")
